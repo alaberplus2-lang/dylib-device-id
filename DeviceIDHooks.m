13ce931c79ba8dd4d8a0f1f611f99ccac654abac
alaberplus2-lang <alaberplus2@gmail.com>
2026-07-14 14:02:51 +0300

    Create missing DeviceIDHooks.m file

---
+++ DeviceIDHooks.m
@@ -0,0 +1,11 @@
+#import "DeviceIDHooks.h"
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+
+@implementation DeviceIDHooks
+
++ (void)setupAllHooks {
+    NSLog(@"[DeviceIDSpoofer] Setting up device ID hooks...");
+}
+
+@end
